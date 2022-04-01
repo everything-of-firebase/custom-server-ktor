b9538dbcfe3ed96ac52656c5010833576afab6da
유원석 <wonsuc@cnspartner.com>
2022-04-01 17:03:14 +0900

    리소스에서 React 제거 (추후에 독립된 경로로 분리해서 다시 추가할 예정)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import com.github.gradle.node.yarn.task.YarnTask
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 val ktor_version: String by project
 val kotlin_version: String by project
 val firebase_version: String by project
@@ -9,7 +6,6 @@ val logback_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.6.10"
-    id("com.github.node-gradle.node") version "3.2.1"
 }
 
 group = "app.pwdr"
@@ -45,43 +41,4 @@ dependencies {
     // Test
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-}
-
-// REF: https://dev.to/darkes/using-gradle-s-kotlin-dsl-to-bundle-react-with-a-spring-boot-web-application-355k
-// REF: https://github.com/node-gradle/gradle-node-plugin/blob/master/src/test/resources/fixtures/kotlin/build.gradle.kts
-val appYarnInstallTask = tasks.register<YarnTask>("appYarnInstall") {
-    description = "Installs all dependencies from package.json"
-    workingDir.set(file("${project.projectDir}/src/main/resources/react"))
-    args.set(listOf("install"))
-}
-
-val appYarnBuildTask = tasks.register<YarnTask>("appYarnBuild") {
-    dependsOn(appYarnInstallTask)
-    description = "Builds project"
-    workingDir.set(file("${project.projectDir}/src/main/resources/react"))
-    args.set(listOf("run", "build"))
-}
-
-val copyWebAppTask = tasks.register<Copy>("copyWebApp") {
-    dependsOn(appYarnBuildTask)
-    description = "Copies built project to where it will be served"
-    from("src/main/resources/react/build")
-    into("build/resources/main/static/.")
-}
-
-node {
-    download.set(true)
-    version.set("16.14.2")
-    yarnVersion.set("1.22.18")
-    workDir.set(file("${project.buildDir}/nodejs"))
-    yarnWorkDir.set(file("${project.buildDir}/yarn"))
-}
-
-tasks.withType<KotlinCompile> {
-    // So that all the tasks run with ./gradlew build
-    dependsOn(copyWebAppTask)
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
-    }
 }
